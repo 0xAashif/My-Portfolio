4a75acf451c8892358915076b47bb8fec77f81dd
0xAashif <aashifkpp@gmail.com>
2026-03-14 08:27:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-document.querySelectorAll('a').forEach(link=>{
-link.addEventListener('click',function(e){
-
-if(this.hash!==""){
-
+document.querySelectorAll("nav a").forEach(link=>{
+link.addEventListener("click",e=>{
+if(link.hash!==""){
 e.preventDefault();
-
-document.querySelector(this.hash).scrollIntoView({
+document.querySelector(link.hash).scrollIntoView({
 behavior:"smooth"
 });
-
 }
-
 });
 });
