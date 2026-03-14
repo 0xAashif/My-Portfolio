6ac74449082832d7082d4c8f2022daa069fc2491
Aloof <aashifkpp@gmail.com>
2026-03-14 06:30:54 +0530

    My portfolio

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-document.querySelectorAll('a').forEach(link => {
-
-link.addEventListener('click', function(e){
-
-if(this.hash !== ''){
-
-e.preventDefault()
-
-document.querySelector(this.hash).scrollIntoView({
-
-behavior:'smooth'
-
-})
-
+document.querySelectorAll('a').forEach(link=>{
+link.addEventListener('click',e=>{
+if(link.hash!==""){
+e.preventDefault();
+document.querySelector(link.hash).scrollIntoView({
+behavior:"smooth"
+});
 }
-
-})
-
-})
+});
+});
